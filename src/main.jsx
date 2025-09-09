4d5f3579006d01cb10003035f285c4040e82dd98
vishxlkr <vishxlkr@gmail.com>
2025-09-09 14:52:53 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ReactGA from "react-ga4";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactGA.initialize("G-1FBNDZ2GT8"); // your Measurement ID
+ReactGA.send("pageview");
+
+createRoot(document.getElementById("root")).render(
+   <StrictMode>
+      <App />
+   </StrictMode>
+);
